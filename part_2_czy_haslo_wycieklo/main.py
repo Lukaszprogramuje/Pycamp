c18969cc94877c4cca7aa49ef73fbf90c8ac7f43
Lukasz Krysiak <lukaszdss@gmail.com>
2022-04-07 14:36:15 +0000

    program napisany obiektowo, w pełni działający

--- part_2_czy_haslo_wycieklo/main.py
+++ part_2_czy_haslo_wycieklo/main.py
@@ -1,51 +1,64 @@
 from requests import get
 import string
 from hashlib import sha1
+import logging
 
+logging.basicConfig(filename='log.txt', filemode='w', level=logging.DEBUG)
 
-hasla = []
-with open("hasla.txt") as file:
+class Password:
+    def __init__(self, password):
+        self.password = password
+        self.password_hash = sha1(self.password.encode("UTF8")).hexdigest()
+        self.prefix = self.password_hash[:5]
+        self.suffix = self.password_hash[5:]
+
+    def check_password_difficulty(self):
+        self.lower = 0
+        self.upper = 0
+        self.punct = 0
+        self.digit = 0
+        for i in self.password:
+            if i in string.ascii_lowercase:
+                self.lower += 1
+            if i in string.ascii_uppercase:
+                self.upper += 1
+            if i in string.punctuation:
+                self.punct += 1
+            if i in string.digits:
+                self.digit += 1
+        if (self.lower * self.upper * self.punct * self.digit != 0) and len(self.password) >= 8:
+            logging.info(f'hasło "{self.password}" spełnia wymagania')
+            return True
+        logging.info(f'hasło "{self.password}" niespełnia wymagań')
+        if len(self.password) < 8:
+            logging.info("hasło za krótkie")
+        if self.lower == 0:
+            logging.info("brakuje małych liter")
+        if self.upper == 0:
+            logging.info("brakuje dużych liter")
+        if self.digit == 0:
+            logging.info("brakuje cyfr")
+        if self.punct == 0:
+            logging.info("brakuje znaków specjalnych")
+        return False
+
+    def check_in_api(self):
+        response = get(f"https://api.pwnedpasswords.com/range/{self.prefix}")
+        counter = response.text.splitlines()
+        for i in counter:
+            if self.suffix in i.lower().split(":"):
+                logging.info(f'hasło "{self.password}" wyciekło {i[36]} razy')
+                return False
+        logging.info(f'hasło "{self.password}" nie wyciekło')
+        return True
+
+passwords = []
+with open("hasla.txt", encoding="UTF8") as file, open("bezpieczne.txt", mode="w", encoding="UTF8") as out:
     for i in file:
-        hasla.append(i.strip())
-
-# string.ascii_lowercase
-# string.ascii_uppercase
-# string.punctuation
-# string.digits
-for haslo in hasla:
-    lower = 0
-    upper = 0
-    punct = 0
-    digit = 0
-    print(haslo)
-    print(f"dlugosc hasla {len(haslo)}")
-    for i in haslo:
-        if i in string.ascii_lowercase:
-            lower += 1
-        if i in string.ascii_uppercase:
-            upper += 1
-        if i in string.punctuation:
-            punct += 1
-        if i in string.digits:
-            digit += 1
-    if lower * upper * punct * digit != 0:
-        print("haslo mocne")
-    else:
-        print("haslo slabe")
-    if lower == 0:
-        print("brakuje malych liter")
-    if upper == 0:
-        print("brakuje duzych liter")
-    if digit == 0:
-        print("brakuje cyfr")
-    if punct == 0:
-        print("brakuje znakow specjalnych")
-    hashowane = sha1(haslo.encode("UTF8"))
-    print(hashowane.hexdigest())
-    print()
-
-
-
-
-
-
+        i = Password(i.strip())
+        logging.info(f'sprawdzam hasło "{i.password}"')
+        passwords.append(i)
+        api = i.check_in_api()
+        dif = i.check_password_difficulty()
+        if api and dif:
+            out.write(str(i.password) + "\n")
