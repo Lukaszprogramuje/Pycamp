6055bb22a9d2c7846234b26d657d31199a7e118b
Lukasz Krysiak <lukaszdss@gmail.com>
2022-04-20 15:57:05 +0000

    Complete mail sender

--- part_3_maile_z_przypomnieniem/main.py
+++ part_3_maile_z_przypomnieniem/main.py
@@ -1,10 +1,10 @@
-from sys import argv
-from os import environ,  getenv
+from os import getenv
 import os
-from dotenv import load_dotenv
-import smtplib, ssl
 import sqlite3
+import smtplib
+import ssl
 from datetime import datetime
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -26,16 +26,17 @@ class Database:
         self.connection.commit()
 
     def insert(self):
-        name = input("podaj imie")
-        email = input("podaj email")
-        book =  input("podaj tytuł książki")
-        return_at = input("kiedy ma być zwrócona, format RRRR-MM-DD GG:MM:SS")
-        self.cursor.execute(f"INSERT INTO loans (id, name, email, book_title, return_at) VALUES (Null, '{name}', '{email}', '{book}', '{return_at}')")
+        name = input("podaj imie  ")
+        email = input("podaj email  ")
+        book =  input("podaj tytuł książki  ")
+        return_at = input("kiedy ma być zwrócona, format RRRR-MM-DD  ")
+        self.cursor.execute(f"""INSERT INTO loans (id, name, email, book_title, return_at)
+        VALUES (Null, '{name}', '{email}', '{book}', '{return_at}')""")
         self.connection.commit()
 
     def expired(self):
-        date_now = datetime.now()
-        return self.cursor.execute(f'SELECT * FROM loans WHERE return_at > {date_now}')
+        date_now = datetime.now().strftime('%Y-%m-%d')
+        return self.cursor.execute(f"SELECT * FROM loans WHERE return_at < '{date_now}'")
 
     def get_all(self):
         return self.cursor.execute('SELECT * FROM loans')
@@ -52,12 +53,20 @@ if os.path.exists(getenv("DB_NAME")) == False:
     db.create_table()
 
 db = Database(getenv("DB_NAME"))
+
+smtp_server = "smtp.gmail.com"
+port = 465
+sender_email = getenv("EMAIL_PYCAMP")
+password = getenv("PASSWORD_PYCAMP")
+context = ssl.create_default_context()
+
 while True:
     print("co chcesz zrobić:")
     print("1. wyświetl rekordy w bazie")
     print("2. dodaj nowy rekord")
     print("3. usuń rekord")
-    print("4. wyjście")
+    print("4. wyślij maile z przypomnieniem")
+    print("5. wyjście")
     decision = int(input())
     if decision == 1:
         dane = db.get_all()
@@ -69,17 +78,16 @@ while True:
         id = int(input("podaj id rekordu do usunięcia"))
         db.gives_back(id)
     elif decision == 4:
+        today = db.expired()
+        for i in today:
+            name = i[1]
+            email_to = i[2]
+            book_title = i[3]
+            message = f"""Subject: Przypomnienie o zwrocie {book_title}\n\n
+            hej {name} powinieneś oddać {book_title}"""
+            with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+                server.login(sender_email, password)
+                server.sendmail(sender_email, email_to, message.encode("UTF8"))
+                print(f"mail z przypomnieniem wysłany do {name}")
+    elif decision == 5:
         break
-
-
-
-
-# smtp_server = "smtp.gmail.com"
-# port = 465
-# sender_email = getenv("EMAIL_PYCAMP")
-# password = getenv("PASSWORD_PYCAMP")
-
-# context = ssl.create_default_context()
-# with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-#     server.login(sender_email, password)
-#     server.sendmail(sender_email, sender_email, "kolejny mail z pythona")
